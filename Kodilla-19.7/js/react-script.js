8c4ecc9f5415c5793bb498d1417a5bb51d06d47d
Bartek Bugała <bartek.bugala@gmail.com>
2019-08-25 16:01:41 +0200

    add Components

--- Kodilla-19.7/js/react-script.js
+++ Kodilla-19.7/js/react-script.js
@@ -1,80 +1,117 @@
 class Stopwatch extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-          running : false,
-          currentTime: {
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0
-          }
-        }
-        this.start = this.start.bind(this);
-        this.stop = this.stop.bind(this);
-        this.reset = this.reset.bind(this);
+  constructor() {
+    super();
+    this.state = {
+      running: false,
+      currentTime: {
+        minutes: 0,
+        seconds: 0,
+        miliseconds: 0
       }
-     
-      start() {
-        if (!this.state.running) {
-          this.state.running = true;
-          this.watch = setInterval(() => this.updateTime(), 10);
-        }
-      }
-          
-      stop() {
-        this.state.running = false;
-        clearInterval(this.watch);
-      }
-
-      updateTime() {    
-        if (!this.state.running) return;
-        this.calculate();
-      }
-
-      toZero() {
-        this.state.currentTime = {
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0
-          };
-      }
-    
-      reset() {
-        this.stop();
-        this.toZero();
-        this.setState({currentTime: {
-          minutes: 0,
-          seconds: 0,
-          miliseconds: 0
-        }})
-
-      }
-    
-      calculate() {
-        const tempTime = this.state.currentTime;
-        tempTime.miliseconds++;
-        if (tempTime.miliseconds >= 100) {
-          tempTime.seconds++;
-          tempTime.miliseconds = 0;
-        }
-        if (tempTime.seconds >= 60) {
-          tempTime.minutes ++;
-          tempTime.seconds = 0;
-        }
-        this.setState({currentTime: tempTime})
-      }
-    
-
-      render() {
-        return <div className='stopwatch'>
-          <div id={'timer'}>{format(this.state.currentTime)}</div>
-          <nav className={'controls'}>
-            <a href={'#'} className={'button'} onClick={this.start} id={'start'}>►</a>
-            <a href={'#'} className={'button'} onClick={this.stop} id={'stop'}>| |</a>
-            <a href={'#'} className={'button'} onClick={this.reset} id={'reset'}>■</a>
-        </nav>      
-        </div>
     }
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  start() {
+    if (!this.state.running) {
+      this.state.running = true;
+      this.watch = setInterval(() => this.updateTime(), 10);
+    }
+  }
+
+  stop() {
+    this.state.running = false;
+    clearInterval(this.watch);
+  }
+
+  updateTime() {
+    if (!this.state.running) return;
+    this.calculate();
+  }
+
+  toZero() {
+    this.state.currentTime = {
+      minutes: 0,
+      seconds: 0,
+      miliseconds: 0
+    };
+  }
+
+  reset() {
+    this.stop();
+    this.toZero();
+    this.setState({
+      currentTime: {
+        minutes: 0,
+        seconds: 0,
+        miliseconds: 0
+      }
+    })
+
+  }
+
+  calculate() {
+    const tempTime = this.state.currentTime;
+    tempTime.miliseconds++;
+    if (tempTime.miliseconds >= 100) {
+      tempTime.seconds++;
+      tempTime.miliseconds = 0;
+    }
+    if (tempTime.seconds >= 60) {
+      tempTime.minutes++;
+      tempTime.seconds = 0;
+    }
+    this.setState({ currentTime: tempTime })
+  }
+
+
+  render() {
+    return <div className='stopwatch'>
+      <div id={'timer'}>{format(this.state.currentTime)}</div>
+      <Controls onStart={this.start} onStop={this.stop} onReset={this.reset} />
+    </div>
+  }
+}
+class Controls extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <nav className={'controls'}>
+      <StartBtn onStart={this.props.onStart} />
+      <StopBtn onStop={this.props.onStop} />
+      <ResetBtn onReset={this.props.onReset} />
+    </nav>
+  }
+}
+
+class StartBtn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <a href={'#'} className={'button'} onClick={this.props.onStart} id={'start'}>►</a>
+  }
+}
+
+class StopBtn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <a href={'#'} className={'button'} onClick={this.props.onStop} id={'stop'}>| |</a>
+  }
+}
+
+class ResetBtn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <a href={'#'} className={'button'} onClick={this.props.onReset} id={'reset'}>■</a>
+  }
 }
 
 function pad0(value) {
