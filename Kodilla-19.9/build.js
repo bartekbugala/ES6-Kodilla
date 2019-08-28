0ddf5821f8f8c8a4d40a9689facf8375a8062a4c
Bartek Bugała <bartek.bugala@gmail.com>
2019-08-28 22:03:41 +0200

    styling

--- Kodilla-19.9/build.js
+++ Kodilla-19.9/build.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -8,13 +8,19 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var App = function (_React$Component) {
-  _inherits(App, _React$Component);
+var myStyle = 'body {\n  background-color: lightblue;\n}\ndiv {\nbackground-color: lightgrey;\n}\n.user-list {\ndisplay: flex;\nflex-wrap: wrap;\n}\n.user-list__user {\ndisplay: flex;\nflex-direction: column;\npadding: 5px;\nborder: solid 2px blue;\nborder-radius: 5px;\nmargin: 5px;\n}\n.user-list__user a {\ntext-decoration: none;\ntext-align: center;\n}\n\n';
 
-  function App() {
-    _classCallCheck(this, App);
+var formStyle = {
+  padding: '10px'
+};
 
-    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this));
+var GithubSearch = function (_React$Component) {
+  _inherits(GithubSearch, _React$Component);
+
+  function GithubSearch() {
+    _classCallCheck(this, GithubSearch);
+
+    var _this = _possibleConstructorReturn(this, (GithubSearch.__proto__ || Object.getPrototypeOf(GithubSearch)).call(this));
 
     _this.state = {
       searchText: '',
@@ -23,20 +29,20 @@ var App = function (_React$Component) {
     return _this;
   }
 
-  _createClass(App, [{
-    key: "onChangeHandle",
+  _createClass(GithubSearch, [{
+    key: 'onChangeHandle',
     value: function onChangeHandle(event) {
       this.setState({ searchText: event.target.value });
     }
   }, {
-    key: "onSubmit",
+    key: 'onSubmit',
     value: function onSubmit(event) {
       var _this2 = this;
 
       event.preventDefault();
       var searchText = this.state.searchText;
 
-      var url = "https://api.github.com/search/users?q=" + searchText;
+      var url = 'https://api.github.com/search/users?q=' + searchText;
       fetch(url).then(function (response) {
         return response.json();
       }).then(function (responseJson) {
@@ -44,26 +50,26 @@ var App = function (_React$Component) {
       });
     }
   }, {
-    key: "render",
+    key: 'render',
     value: function render() {
       var _this3 = this;
 
       return React.createElement(
-        "div",
-        null,
+        'div',
+        { style: formStyle },
         React.createElement(
-          "form",
+          'form',
           { onSubmit: function onSubmit(event) {
               return _this3.onSubmit(event);
             } },
           React.createElement(
-            "label",
-            { htmlFor: "searchText" },
-            "Search by user name"
+            'label',
+            { htmlFor: 'searchText' },
+            'Search by user name'
           ),
-          React.createElement("input", {
-            type: "text",
-            id: "searchText",
+          React.createElement('input', {
+            type: 'text',
+            id: 'searchText',
             onChange: function onChange(event) {
               return _this3.onChangeHandle(event);
             },
@@ -74,7 +80,7 @@ var App = function (_React$Component) {
     }
   }]);
 
-  return App;
+  return GithubSearch;
 }(React.Component);
 
 var UsersList = function (_React$Component2) {
@@ -87,16 +93,16 @@ var UsersList = function (_React$Component2) {
   }
 
   _createClass(UsersList, [{
-    key: "render",
+    key: 'render',
     value: function render() {
       return React.createElement(
-        "div",
-        null,
+        'div',
+        { className: 'user-list' },
         this.users
       );
     }
   }, {
-    key: "users",
+    key: 'users',
     get: function get() {
       return this.props.users.map(function (user) {
         return React.createElement(User, { key: user.id, user: user });
@@ -117,15 +123,15 @@ var User = function (_React$Component3) {
   }
 
   _createClass(User, [{
-    key: "render",
+    key: 'render',
     value: function render() {
       return React.createElement(
-        "div",
-        null,
-        React.createElement("img", { src: this.props.user.avatar_url, style: { maxWidth: '100px' } }),
+        'div',
+        { className: 'user-list__user' },
+        React.createElement('img', { src: this.props.user.avatar_url, style: { maxWidth: '100px' } }),
         React.createElement(
-          "a",
-          { href: this.props.user.html_url, target: "_blank" },
+          'a',
+          { href: this.props.user.html_url, target: '_blank' },
           this.props.user.login
         )
       );
@@ -133,6 +139,34 @@ var User = function (_React$Component3) {
   }]);
 
   return User;
+}(React.Component);
+
+var App = function (_React$Component4) {
+  _inherits(App, _React$Component4);
+
+  function App() {
+    _classCallCheck(this, App);
+
+    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+  }
+
+  _createClass(App, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'style',
+          null,
+          myStyle
+        ),
+        React.createElement(GithubSearch, null)
+      );
+    }
+  }]);
+
+  return App;
 }(React.Component);
 
 ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
--- Kodilla-19.9/script.js
+++ Kodilla-19.9/script.js
@@ -1,69 +1,108 @@
+const myStyle = 
+`body {
+  background-color: lightblue;
+}
+div {
+background-color: lightgrey;
+}
+.user-list {
+display: flex;
+flex-wrap: wrap;
+}
+.user-list__user {
+display: flex;
+flex-direction: column;
+padding: 5px;
+border: solid 2px blue;
+border-radius: 5px;
+margin: 5px;
+}
+.user-list__user a {
+text-decoration: none;
+text-align: center;
+}
 
-class App extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        searchText: '',
-        users: []
-      };
-    }
-  
-    onChangeHandle(event) {
-      this.setState({searchText: event.target.value});
-    }
-  
-    onSubmit(event) {
-      event.preventDefault();
-      const {searchText} = this.state;
-      const url = `https://api.github.com/search/users?q=${searchText}`;
-      fetch(url)
-        .then(response => response.json())
-        .then(responseJson => this.setState({users: responseJson.items}));
-    }
-  
-    render() {
-      return (
-        <div>
-          <form onSubmit={event => this.onSubmit(event)}>
-            <label htmlFor="searchText">Search by user name</label>
-            <input
-              type="text"
-              id="searchText"
-              onChange={event => this.onChangeHandle(event)}
-              value={this.state.searchText}/>
-          </form>
-          <UsersList users={this.state.users}/>
-        </div>
-      );
-    }
+`;
+
+const formStyle = {
+  padding: '10px'
+};
+
+class GithubSearch extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      users: []
+    };
   }
+
+  onChangeHandle(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const { searchText } = this.state;
+    const url = `https://api.github.com/search/users?q=${searchText}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(responseJson => this.setState({ users: responseJson.items }));
+  }
+
+  render() {
+    return (
+      <div style={formStyle}>
+        <form onSubmit={event => this.onSubmit(event)}>
+          <label htmlFor="searchText">Search by user name</label>
+          <input
+            type="text"
+            id="searchText"
+            onChange={event => this.onChangeHandle(event)}
+            value={this.state.searchText} />
+        </form>
+        <UsersList users={this.state.users} />
+      </div>
+    );
+  }
+}
 
 class UsersList extends React.Component {
-    get users() {
-      return this.props.users.map(user => <User key={user.id} user={user}/>);
-    }
-  
-    render() {
-      return (
-        <div>
-          {this.users}
-        </div>
-      );
-    }
+  get users() {
+    return this.props.users.map(user => <User key={user.id} user={user} />);
   }
+
+  render() {
+    return (
+      <div className="user-list">
+        {this.users}
+      </div>
+    );
+  }
+}
 
 class User extends React.Component {
-    render() {
-      return (
-        <div>
-          <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
-          <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="user-list__user">
+        <img src={this.props.user.avatar_url} style={{ maxWidth: '100px' }} />
+        <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+      </div>
+    );
   }
+}
 
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+class App extends React.Component {
+  render() {
+    return <div>
+      <style>{myStyle}</style>
+      <GithubSearch />
+    </div>
+
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
